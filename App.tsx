c8cd7d445a3f5a12b92bb81b2d8a84a674367a63
Sebastien Bailouni <sbailouni@gmail.com>
2022-04-13 15:43:37 +1000

    Setup RNGH properly

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useFonts } from "expo-font";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 // screens
 import HomeScreen from "screens/HomeScreen";
 import MoodMatrix from "screens/MoodMatrix";
@@ -26,54 +27,56 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="VerticalSurvey"
-          component={VerticalSurveyScreen}
-          options={{
-            headerLeft: BackButton,
-            headerTitle: "",
-            headerStyle: { backgroundColor: "transparent" },
-          }}
-        />
-        <Stack.Screen
-          name="MoodMatrix"
-          component={MoodMatrix}
-          options={{
-            headerLeft: BackButton,
-            headerTitle: "",
-            headerStyle: { backgroundColor: "transparent" },
-          }}
-        />
-        <Stack.Screen
-          name="MoodMatrixGradient"
-          component={MoodMatrixGradient}
-          options={{
-            headerLeft: BackButton,
-            headerTitle: "",
-            headerStyle: { backgroundColor: "transparent" },
-          }}
-        />
-        <Stack.Screen
-          name="HifiIntro"
-          component={HifiIntro}
-          options={{
-            headerLeft: BackButton,
-            headerTitle: "",
-            headerShadowVisible: false,
-            headerBackTitle: "",
-            headerStyle: {
-              backgroundColor: "transparent",
-            },
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="VerticalSurvey"
+            component={VerticalSurveyScreen}
+            options={{
+              headerLeft: BackButton,
+              headerTitle: "",
+              headerStyle: { backgroundColor: "transparent" },
+            }}
+          />
+          <Stack.Screen
+            name="MoodMatrix"
+            component={MoodMatrix}
+            options={{
+              headerLeft: BackButton,
+              headerTitle: "",
+              headerStyle: { backgroundColor: "transparent" },
+            }}
+          />
+          <Stack.Screen
+            name="MoodMatrixGradient"
+            component={MoodMatrixGradient}
+            options={{
+              headerLeft: BackButton,
+              headerTitle: "",
+              headerStyle: { backgroundColor: "transparent" },
+            }}
+          />
+          <Stack.Screen
+            name="HifiIntro"
+            component={HifiIntro}
+            options={{
+              headerLeft: BackButton,
+              headerTitle: "",
+              headerShadowVisible: false,
+              headerBackTitle: "",
+              headerStyle: {
+                backgroundColor: "transparent",
+              },
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
